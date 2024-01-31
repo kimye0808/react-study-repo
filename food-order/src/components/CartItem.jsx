c7a158db7982cf3e9452eb65d77c3789e6699404
kimye0808 <kyh011@hanyang.ac.kr>
2024-01-31 17:32:15 +0900

    add: food-order

---
+++ food-order/src/components/CartItem.jsx
@@ -0,0 +1,23 @@
+import { currencyFormatter } from "../util/formatting";
+
+//카트에 있는 아이템 컴포넌트
+export default function CartItem({
+  name,
+  quantity,
+  price,
+  onIncrease,
+  onDecrease,
+}) {
+  return (
+    <li className="cart-item">
+      <p>
+        {name} - {quantity} X {currencyFormatter.format(price)}
+      </p>
+      <p className="cart-item-actions">
+        <button onClick={onDecrease}>-</button>
+        <span>{quantity}</span>
+        <button onClick={onIncrease}>+</button>
+      </p>
+    </li>
+  );
+}
---
+++ food-order/src/components/Error.jsx
@@ -0,0 +1,9 @@
+//error 컴포넌트
+export default function Error({ title, message }) {
+  return (
+    <div className="error">
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
